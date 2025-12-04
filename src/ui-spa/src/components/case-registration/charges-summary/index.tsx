c7cbc4e4a790d0b82c16528bc6573bea273a3af9
renjithabby <renjithabby@gmail.com>
2025-12-04 20:09:51 +0000

    another partial commit

---
+++ src/ui-spa/src/components/case-registration/charges-summary/index.tsx
@@ -0,0 +1,154 @@
+import {
+  useRef,
+  useEffect,
+  useState,
+  // useContext,
+  useCallback,
+  useMemo,
+} from "react";
+import { Radios, Button, ErrorSummary, BackLink } from "../../govuk";
+// import { CaseRegistrationFormContext } from "../../../common/providers/CaseRegistrationProvider";
+import { useNavigate } from "react-router-dom";
+import styles from "../index.module.scss";
+import pageStyles from "./index.module.scss";
+
+const SuspectSummaryPage = () => {
+  type ErrorText = {
+    errorSummaryText: string;
+    inputErrorText?: string;
+  };
+  type FormDataErrors = {
+    addMoreChargesRadio?: ErrorText;
+  };
+  const errorSummaryRef = useRef<HTMLInputElement>(null);
+  // const { state } = useContext(CaseRegistrationFormContext);
+  const navigate = useNavigate();
+
+  const [addMoreChargesRadio, setAddMoreChargesRadio] = useState<string>("");
+
+  const [formDataErrors, setFormDataErrors] = useState<FormDataErrors>({});
+
+  const errorSummaryProperties = useCallback(
+    (errorKey: keyof FormDataErrors) => {
+      if (errorKey === "addMoreChargesRadio") {
+        return {
+          children: formDataErrors[errorKey]?.errorSummaryText,
+          href: "#add-more-charges-radio-yes",
+          "data-testid": "add-more-charges-radio-yes",
+        };
+      }
+      return null;
+    },
+    [formDataErrors],
+  );
+
+  const validateFormData = () => {
+    const errors: FormDataErrors = {};
+    let isValid = true;
+
+    if (!addMoreChargesRadio) {
+      errors.addMoreChargesRadio = {
+        errorSummaryText: "Please select an option",
+        inputErrorText: "Please select an option",
+      };
+      isValid = false;
+    }
+
+    setFormDataErrors(errors);
+    return isValid;
+  };
+
+  const errorList = useMemo(() => {
+    const validErrorKeys = Object.keys(formDataErrors).filter(
+      (errorKey) => formDataErrors[errorKey as keyof FormDataErrors],
+    );
+
+    const errorSummary = validErrorKeys.map((errorKey, index) => ({
+      reactListKey: `${index}`,
+      ...errorSummaryProperties(errorKey as keyof FormDataErrors)!,
+    }));
+
+    return errorSummary;
+  }, [formDataErrors, errorSummaryProperties]);
+
+  useEffect(() => {
+    if (errorList.length) errorSummaryRef.current?.focus();
+  }, [errorList]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!validateFormData()) return;
+
+    if (addMoreChargesRadio === "yes") {
+      navigate(`/case-registration/add-charge-suspect`);
+      return;
+    }
+
+    navigate("/case-registration/case-complexity");
+  };
+
+  return (
+    <div className={pageStyles.caseSuspectsSummaryPage}>
+      <BackLink to={`/case-registration/want-to-add-charges`}>Back</BackLink>
+      {!!errorList.length && (
+        <div
+          ref={errorSummaryRef}
+          tabIndex={-1}
+          className={styles.errorSummaryWrapper}
+        >
+          <ErrorSummary
+            data-testid={"case-suspect-Aliases-error-summary"}
+            errorList={errorList}
+            titleChildren="There is a problem"
+          />
+        </div>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1>{`You have added  charges`}</h1>
+
+        <div className={styles.inputWrapper}>
+          <Radios
+            className="govuk-radios--inline"
+            fieldset={{
+              legend: {
+                children: <h2>Do you need to add another charge? </h2>,
+              },
+            }}
+            errorMessage={
+              formDataErrors["addMoreChargesRadio"]
+                ? {
+                    children:
+                      formDataErrors["addMoreChargesRadio"].errorSummaryText,
+                  }
+                : undefined
+            }
+            items={[
+              {
+                id: `add-more-charges-radio-yes`,
+                children: "Yes",
+                value: "yes",
+                "data-testid": `add-more-charges-radio-yes`,
+              },
+              {
+                id: `add-more-charges-radio-no`,
+                children: "No",
+                value: "no",
+                "data-testid": `add-more-charges-radio-no`,
+              },
+            ]}
+            value={addMoreChargesRadio}
+            onChange={(value) => {
+              if (value) setAddMoreChargesRadio(value);
+            }}
+          ></Radios>
+        </div>
+        <Button type="submit" onClick={() => handleSubmit}>
+          Save and Continue
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default SuspectSummaryPage;
