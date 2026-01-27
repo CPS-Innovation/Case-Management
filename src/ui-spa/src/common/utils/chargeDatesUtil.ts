38d0bc3409b31ad436bfbccf9474192fb08966ba
renjithabby <renjithabby@gmail.com>
2026-01-27 17:37:51 +0000

    added unit tests

--- src/ui-spa/src/common/utils/chargeDatesUtil.ts
+++ src/ui-spa/src/common/utils/chargeDatesUtil.ts
@@ -1,6 +1,5 @@
 import { type SuspectFormData } from "../reducers/caseRegistrationReducer";
 import { getChargesSummaryList } from "./getChargesSummaryList";
-import { isValidOnOrBeforeDate } from "./isValidOnOrBeforeDate";
 import { isValidOnOrAfterDate } from "./isValidOnOrAfterDate";
 
 export const getChargeDates = (suspects: SuspectFormData[]) => {
@@ -8,26 +7,14 @@ export const getChargeDates = (suspects: SuspectFormData[]) => {
   if (chargesList.length === 0) return [];
 
   const charges = chargesList.reduce((acc, item) => {
-    item.charges.every((charge) => {
+    item.charges.forEach((charge) => {
       if (charge.offenceFromDate) acc.push(charge.offenceFromDate);
       if (charge.offenceToDate) acc.push(charge.offenceToDate);
     });
-
     return acc;
   }, [] as string[]);
 
   return charges;
-};
-
-export const isOnOrBeforeChargeDates = (
-  inputDate: string,
-  suspects: SuspectFormData[],
-) => {
-  const charges = getChargeDates(suspects);
-  const invalid = charges.find(
-    (charge) => !isValidOnOrBeforeDate(inputDate, charge),
-  );
-  return !invalid;
 };
 
 export const isOnOrAfterChargeDates = (
