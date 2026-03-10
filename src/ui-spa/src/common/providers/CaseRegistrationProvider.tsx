f20845693f610a35dee562b19da847b49393e40e
renjithabby <renjithabby@gmail.com>
2026-03-10 17:43:54 +0000

    added the  updated state to session staorage and load data from it on refresh

--- src/ui-spa/src/common/providers/CaseRegistrationProvider.tsx
+++ src/ui-spa/src/common/providers/CaseRegistrationProvider.tsx
@@ -11,13 +11,27 @@ const CaseRegistrationFormContext = createContext<{
   dispatch: React.Dispatch<CaseRegistrationActions>;
 }>({ state: initialState, dispatch: () => null });
 
+const loadFromSessionStorage = (): CaseRegistrationState => {
+  console.log("Loading state from sessionStorage");
+  const serializedState = sessionStorage.getItem("caseRegistration");
+  if (!serializedState) return initialState;
+
+  try {
+    const state = JSON.parse(serializedState);
+    return state;
+  } catch (error) {
+    console.error("Error loading state from sessionStorage:", error);
+    return initialState;
+  }
+};
+
 const CaseRegistrationProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   const [state, dispatch] = useReducer(
     caseRegistrationReducer,
     initialState,
-    (init) => init,
+    loadFromSessionStorage,
   );
 
   const contextValue = useMemo(() => ({ state, dispatch }), [state, dispatch]);
--- src/ui-spa/src/components/ProtectedRoutes.tsx
+++ src/ui-spa/src/components/ProtectedRoutes.tsx
@@ -1,8 +1,15 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { CaseRegistrationFormContext } from "../common/providers/CaseRegistrationProvider";
 const ProtectedRoutes = () => {
   const { state } = useContext(CaseRegistrationFormContext);
+  useEffect(() => {
+    const serializedState = JSON.stringify(state);
+    sessionStorage.setItem("caseRegistration", serializedState);
+    const sizeInBytes = new Blob([serializedState]).size;
+    const sizeInMB = sizeInBytes / (1024 * 1024);
+    console.log("Saved state size:", sizeInMB, "MB");
+  }, [state]);
 
   const isAllowed =
     state.formData.suspectDetailsRadio &&
