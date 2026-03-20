0edca5a411e6551b5e3436b1fe92852f29ec8024
renjithabby <renjithabby@gmail.com>
2026-03-20 13:04:20 +0000

    refactored to  class approach to handle each pages easily

---
+++ src/ui-spa/integration-tests/pages/caseAreasPage.ts
@@ -0,0 +1,65 @@
+import { Page, expect } from "@playwright/test";
+
+export class CaseAreasPage {
+  private readonly route = "http://localhost:5173/case-registration/areas";
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async verifyUrl() {
+    await expect(this.page).toHaveURL(this.route);
+  }
+
+  async verifyPageElements() {
+    await expect(this.page.locator("label").first()).toHaveText(
+      "What is the division or area?",
+    );
+    await expect(this.page.locator("#area-or-division-text")).toHaveValue(
+      "CAMBRIDGESHIRE",
+    );
+  }
+
+  async errorValidations() {
+    await this.verifyErrorSummaryClear();
+
+    await this.enterAreaOrDivision("");
+    await this.saveAndContinue();
+    await expect(
+      this.page.getByTestId("case-area-error-summary"),
+    ).toBeVisible();
+    await expect(
+      this.page.getByTestId("area-or-division-text-link"),
+    ).toBeVisible();
+    await expect(
+      this.page.getByTestId("area-or-division-text-link"),
+    ).toHaveText("Select a division or area");
+    await this.enterAreaOrDivision("abc");
+    await this.saveAndContinue();
+    await expect(
+      this.page.getByTestId("case-area-error-summary"),
+    ).toBeVisible();
+    await expect(
+      this.page.getByTestId("area-or-division-text-link"),
+    ).toBeVisible();
+    await expect(
+      this.page.getByTestId("area-or-division-text-link"),
+    ).toHaveText("Select a valid case area");
+  }
+
+  async verifyErrorSummaryClear() {
+    await expect(
+      this.page.getByTestId("case-area-error-summary"),
+    ).not.toBeVisible();
+  }
+
+  async enterAreaOrDivision(name: string) {
+    await this.page.locator("#area-or-division-text").fill(name);
+    await this.page.keyboard.press("Escape");
+  }
+
+  async saveAndContinue() {
+    await this.page.getByRole("button", { name: "Save and continue" }).click();
+  }
+}
